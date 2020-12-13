9b59390da96ff72ee73b3fff35e00ac60648efbc
Ray Gesualdo <ray@raygesualdo.com>
2020-12-12 22:06:05 -0500

    re-implement cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,27 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
+const PROD = process.env.NODE_ENV === 'production'
+
 module.exports = () => ({
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    PROD &&
+      purgecss({
+        content: ['src/*.pug', 'src/*.js'],
+        defaultExtractor: (content) => {
+          // Capture as liberally as possible, including things like `h-(screen-1.5)`
+          const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+          // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+          const innerMatches =
+            content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+          return broadMatches.concat(innerMatches)
+        },
+      }),
+    PROD &&
+      cssnano({
+        preset: 'default',
+      }),
+  ].filter(Boolean),
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: ['src/*.pug', 'src/*.js'],
+  purge: {
+    enabled: false,
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
